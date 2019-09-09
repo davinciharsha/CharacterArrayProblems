fafe8a2bd9d4590fc84b38fb2a9a18263c92ef86
sai yadavalli <davinciharsha@gmail.com>
2019-09-10 00:50:14 +0530

    first problem done

--- CharacterArrayProblems/Program.cs
+++ CharacterArrayProblems/Program.cs
@@ -11,11 +11,24 @@ namespace CharacterArrayProblems
         static void Main(string[] args)
         {
             //step 1: first check in
-            Console.WriteLine("");
+            //Console.WriteLine("");
             //step 2: successfully cloned into local and checked out
             //step 3: now merge this with initial version of code
             //step 4: added as FirstBranch
             //step 5: created SecondBranch and merging with FirstBranch
+            string str = Console.ReadLine();
+            Console.WriteLine(str);
+            char[] str1 = new char[10];
+            str1 = str.ToCharArray();
+            for(int i=0;i<str1.Length-1;i++)
+            {
+                if(str1[i]>str1[i+1])
+                {
+                    Console.WriteLine("Incorrect");
+                }
+                
+            }
+            Console.ReadKey();
         }
     }
 }
